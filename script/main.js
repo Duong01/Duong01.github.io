eb2e1c45b6d3fb3a6d6a5b75f9bf2b72166b652f
Nguyễn Văn Dương <86581230+Duong01@users.noreply.github.com>
2025-04-01 19:07:29 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -127,10 +127,10 @@ const animationTimeline = () => {
     .to(".fake-btn", 0.1, {
         backgroundColor: "rgb(127, 206, 248)",
     },
-    "+=4")
+    "+=2")
     .to(
         ".four",
-        0.3, {
+        0.2, {
             scale: 0.2,
             opacity: 0,
             y: -150
